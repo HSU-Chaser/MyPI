0e087f15ed5dbca3a0c71095e11e9d764a4a98d7
Repump <ojunhy0@gmail.com>
2014-04-13 20:39:46 +0900

    ResultObject Add

---
+++ src/main/ResultObject.java
@@ -0,0 +1,56 @@
+package main;
+
+public class ResultObject {
+	String title = null;
+	String link = null;
+	String displayLink = null;
+	String snippet = null;
+	int resultNumber = 0;
+	int exposure = 0;
+	int reliability = 0;
+
+	public ResultObject(String title, String link, String displayLink,
+			String snippet, int resultNumber) {
+		this.title = title;
+		this.link = link;
+		this.displayLink = displayLink;
+		this.snippet = snippet;
+		this.resultNumber = resultNumber;
+	}
+
+	public int getExposure() {
+		return exposure;
+	}
+
+	public void setExposure(int exposure) {
+		this.exposure = exposure;
+	}
+
+	public int getReliability() {
+		return reliability;
+	}
+
+	public void setReliability(int reliability) {
+		this.reliability = reliability;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public String getDisplayLink() {
+		return displayLink;
+	}
+
+	public String getSnippet() {
+		return snippet;
+	}
+
+	public int getResultNumber() {
+		return resultNumber;
+	}
+}
