2abebed0f72cb65e23cece6e9423253e11e79a82
Yang Si Young <yangsy0714@naver.com>
2014-04-27 18:22:29 +0900

    id,name

--- src/main/patternanalysis/information/Name.java
+++ src/main/patternanalysis/information/Name.java
@@ -1,4 +1,4 @@
-package main.patternanalysis.information;
+﻿package main.patternanalysis.information;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,13 +8,15 @@ public class Name {
 	public static void PatternAnalysis(String file) {
 		// TODO Auto-generated method stub
 		// 한글이름 2글자~5글자
-		Pattern name = Pattern
-				.compile("[가-힣]{2,5}|[\\w\\-]{2,10}\\s[\\w\\-]{2,10}\\s[\\w\\-]{2,10}");
-		// 영문이름 ex) si-young yang OR yang si-young OR siyoung yang OR yang
-		// siyoung OR yang si young
-		Matcher m = name.matcher(file);
-		while (m.find())
-			System.out.println(m.group());
+				Pattern name = Pattern.compile("([가간갈감강견경고공곽구국군권기길김나난남낭노"
+						+ "도돈독마매맹명박반방배백범변복봉부빈사삼상서석선설"
+						+ "섭성소손송수순승시신심아안양엄연염예오옥온왕용우운원위유육윤"
+						+ "은이인임자장전제조종주지진차창채천최추춘탁태"
+						+ "판팽편평포표풍피필하학한함해허현형호홍황보])[가-힣]{1,5}");
+				// 영문이름 ex) si-young yang OR yang si-young OR siyoung yang OR yang siyoung OR yang si young
+				// 영문이름은 일단 보류 |[\\w\\-]{2,10}\\s[\\w\\-]{2,10}\\s[\\w\\-]{2,10}
+				Matcher m = name.matcher(file);
+				while(m.find()) System.out.println(m.group());
 	}
 
 }
