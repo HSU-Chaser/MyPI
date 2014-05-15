c034c9b5c6f846c9656ea4dfc9814aef14d38e56
Yang Si Young <yangsy0714@naver.com>
2014-05-15 17:45:31 +0900

    pattern list

---
+++ src/information/BirthDay.java
@@ -0,0 +1,20 @@
+﻿package information;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BirthDay {
+	public static List<String> birthdayList = new ArrayList<String>();
+	
+	public static void PatternAnalysis(String file) {
+		// TODO Auto-generated method stub
+		Pattern birth = Pattern.compile("[0-9]{2}(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[01])|19[0-9]{2}년\\s[0-9]{1,2}월"
+				+"\\s[0-9]{1,2}일 |20[0-9]{2}년\\s[0-9]{1,2}월\\s[0-9]{1,2}일|19[0-9]{2}년[0-9]{1,2}월[0-9]{1,2}일|20[0-9]{2}년"
+				+ "\\s[0-9]{1,2}월\\s[0-9]{1,2}일|[0-9]{1,2}월\\s[0-9]{1,2}일|(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[01])");
+		Matcher m = birth.matcher(file);
+		while(m.find()) birthdayList.add(m.group()); //System.out.println(m.group());
+	}
+
+}
---
+++ src/information/HandPhone.java
@@ -0,0 +1,17 @@
+﻿package information;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HandPhone{
+	public static List<String> handphoneList = new ArrayList<String>();
+	
+	public static void PatternAnalysis(String file) {
+		// TODO Auto-generated method stub
+		Pattern handphone = Pattern.compile("01[016789]+[\\d{3,4}]+[\\d{4}]|01[016789]+[-\\d{3,4}]+[-\\d{4}]|01[016789]+[.\\d{3,4}]+[.\\d{4}]"); //(\\d{3})(\\d{3,4})(\\d{4})
+		Matcher m  = handphone.matcher(file);
+		while(m.find()) handphoneList.add(m.group());//System.out.println(m.group());		
+	}
+}
