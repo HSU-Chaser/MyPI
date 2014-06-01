18a23705f8e1bd01f05c9e637336c74c50bb1f1e
tera16 <tera16@naver.c60>
2014-06-01 17:31:23 +0900

    after merge, RankingCount +

---
+++ src/main/patternanalysis/RankingCount.java
@@ -0,0 +1,20 @@
+package main.patternanalysis;
+
+public class RankingCount {
+
+	
+	public static int emailCount = 0;
+	public static int cellphoneCount = 0;
+	public static int birthdayCount = 0;
+	public static int addressCount = 0;
+	public static int residentCount = 0;
+	public static int homephoneCount = 0;
+	public static int idCount = 0;
+	public static int nameCount = 0;
+	public static int schoolCount = 0;
+	public static int nicknameCount = 0;
+	public static int workplaceCount = 0;
+	public static int occupationCount = 0 ;
+
+	
+}
